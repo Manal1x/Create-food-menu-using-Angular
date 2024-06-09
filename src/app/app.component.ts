eedb807238c8e949866440c022f5227408c698cc
Manal1x <Manal1.34n@gmail.com>
2024-06-09 19:19:24 +0300

    Menu project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'menuWeekProject';
+
+  total=0;
+  qty=0;
+
+  title = 'menu';
+
+  foodmenu=[
+    {id:1,name:"دجاج",price:30,description:"دجاج مشوي على الفحم",img:"assets/images/cheken.jpg"},
+    {id:2,name:"دجاج",price:30,description:"دجاج شواية ",img:"assets/images/cheken.jpg"},
+    {id:3,name:"ايدام",price:20,description:"ايدام خضار مشكل",img:"assets/images/edam.jpg"},
+    {id:4,name:"ايدام",price:20,description:"ايدام  بالدجاج على الفرن",img:"assets/images/edam2.jpg"}
+  ]
+
+  sum( event: any, price: number ){
+    const isChecked: boolean=event.target.checked;
+    if(isChecked){
+      this.total=this.total+price
+      this.qty+=1;
+    }
+    else{
+      this.total=this.total-price
+      this.qty-=1;
+    }
+  
+  }
+
+  menuName:string="";
+
+  findMenu(menu:any[]){
+    return menu.filter(item=> item.name.toLowerCase().includes( this.menuName.toLocaleLowerCase()));
+  }
 }
